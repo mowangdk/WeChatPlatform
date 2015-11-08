503f20e0d373743a481256e74b3a860e0e7d4ad6
geyingqi <geyingqi0701@foxmail.com>
2015-11-08 21:07:59 +0800

    WeChatPlatform

---
+++ src/controller/WeChatServlet.java
@@ -0,0 +1,64 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class WeChatServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+
+		// 读取接收到的xml信息
+		StringBuffer sb = new StringBuffer();
+		InputStream is = request.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+		String s = "";
+		while ((s = br.readLine()) != null) {
+			sb.append(s);
+		}
+
+		String xml = sb.toString(); // 次为接受到微信端发送过来的xml数据
+
+		String result = "";
+
+		/* 判断是否为微信接入激活验证,只有首次接入才会收到echostr参数,此时需要将它直接返回 */
+
+		String echostr = request.getParameter("echostr");
+		if (null != echostr && echostr.length() > 1) {
+			result = echostr;
+		} else {
+			// 正常的微信处理流程
+			result = new WechatProcess().processWechatMag(xml);
+		}
+		try {
+			OutputStream os = response.getOutputStream();
+			os.write(result.getBytes("UTF-8"));
+			os.flush();
+			os.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request,response);
+	}
+	
+
+}
---
+++ src/controller/WechatProcess.java
@@ -0,0 +1,18 @@
+package controller;
+
+public class WechatProcess {
+
+	public String processWechatMag(String xml) {
+		//解析xml数据
+		ReceiveXmlEntity xmlEntity = new ReceiveXmlProcess().getMsgEntity(xml);
+		
+		//以文本消息为例,调用图灵机器人api接口,获取回复内容
+		String result = "";
+		if ("text".endsWith(xmlEntity.getMsgType())){
+			result = new TulingApiProcess().getTulingResult(xmlEntity.getContent());
+		}
+		
+		result = new FormatXmlProcess().formatXmlAnswer(xmlEntity.getFromUserName(), xmlEntity.getToUserName(), result);
+	}
+
+}
